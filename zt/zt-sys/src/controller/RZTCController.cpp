6b99a8a7bcfe73691eb37d02589572014a28a1a5
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-04-28 00:34:49 +0200

    Cleaning up C++ code for network controller

--- zt/zt-sys/src/controller/RZTCController.cpp
+++ zt/zt-sys/src/controller/RZTCController.cpp
@@ -49,37 +49,6 @@ void RZTCController::request(
 	const Identity &identity,
 	const Dictionary<ZT_NETWORKCONFIG_METADATA_DICT_CAPACITY> &metaData)
 {
-	// CertificateOfMembership *com = new CertificateOfMembership(
- //            		1650367222104,
- //            		123456,
- //            		5124095572525857, // 0x12345678654321
-	// 		identity
-	// 	);
-	// Buffer<1024> *tmp = new Buffer<1024>();
-	// tmp->clear();
-	// com->serialize(*tmp);
-	// const uint8_t *data = static_cast<const uint8_t*>(tmp->data());
-	// for (int i = 0; i < tmp->size(); i++) {
-	// 	std::cout << std::hex << std::setfill('0') << std::setw(2) << uint(data[i]);
-	// }
-	// std::cout << "\n";
-
-	// NetworkConfig *nc = new NetworkConfig();
-	// nc->networkId = 0x12345678654321;
-	// nc->timestamp = 1650367222104;
-	// nc->revision = 1;
-	// nc->issuedTo = identity.address();
-	// memset(nc->name, 0, sizeof(nc->name));
-	// memcpy(nc->name, "test-network0", sizeof(char)*13);
-	//
-	// CertificateOfMembership com(1650367222104,123456,0x12345678654321,identity);
-	// nc->com = com;
-	//
-	// Dictionary<ZT_NETWORKCONFIG_DICT_CAPACITY> *dict = new Dictionary<ZT_NETWORKCONFIG_DICT_CAPACITY>();
-	//
-	// nc->toDictionary(reinterpret_cast<Dictionary<ZT_NETWORKCONFIG_DICT_CAPACITY>&>(*dict), false);
-	// std::cout << dict->data() << "\n";
-
 	_cbs.networkRequestCallback(
 		reinterpret_cast<RZTC_Controller*>(this),
 		_uptr,
@@ -104,28 +73,6 @@ void RZTCController::sendConfig(
 	std::unique_ptr<Dictionary<ZT_NETWORKCONFIG_DICT_CAPACITY>> data(new Dictionary<ZT_NETWORKCONFIG_DICT_CAPACITY>(nc));
 	std::unique_ptr<NetworkConfig> netconf(new NetworkConfig());
 	netconf->fromDictionary(*(data.get()));
-
-	// Buffer<1024> *tmp = new Buffer<1024>();
-	// tmp->clear();
-	// netconf->com.serialize(*tmp);
-	// const uint8_t *com_data = static_cast<const uint8_t*>(tmp->data());
-	// std::cout << "Pre sign" << "\n";
-	// for (int i = 0; i < tmp->size(); i++) {
-	// 	std::cout << std::hex << std::setfill('0') << std::setw(2) << uint(com_data[i]);
-	// }
-	// std::cout << "\n";
-
-	// netconf->com.sign(_signingId);
-
-	// tmp->clear();
-	// netconf->com.serialize(*tmp);
-	// com_data = static_cast<const uint8_t*>(tmp->data());
-	// std::cout << "Post sign" << "\n";
-	// for (int i = 0; i < tmp->size(); i++) {
-	// 	std::cout << std::hex << std::setfill('0') << std::setw(2) << uint(com_data[i]);
-	// }
-	// std::cout << "\n";
-
 
 	_sender->ncSendConfig(nwid, requestPacketId, destAddr, *(netconf.get()), sendLegacyFormat);
 }
